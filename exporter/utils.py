a03f392266d37cfd8cba7cd5fbc5cb90d8265413
ivanhercaz <ivan@ivanhercaz.com>
2018-10-05 13:01:05 +0100

    Utils class with two methods, one to check if a command is installed and another to run the commands

--- exporter/utils.py
+++ exporter/utils.py
@@ -1,7 +1,35 @@
-from subprocess import run
+from subprocess import run, DEVNULL
 from subprocess import CalledProcessError
 
+import sys
+
 class Utils:
-    def runCommand(self, *args):
-        print(args) # to test if the args are fine
-        return run(args)
+    def checkCommand(self, command, **kwargs):
+        if "commandName" in kwargs:
+            commandName = kwargs["commandName"]
+
+        else:
+            commandName = command
+
+        try:
+            run(command, stdout=DEVNULL)
+
+            print("{} installed".format(commandName))
+
+        except FileNotFoundError:
+            print("Error: probably {} isn't installed.".format(commandName))
+            sys.exit()
+
+    def runCommand(self, *args, inputfile="", outputfile=""):
+        print(outputfile)
+        if inputfile is not "":
+            if outputfile is not "": 
+                try:
+                    with open(inputfile, "r") as inputfile, open(outputfile, "w") as outputfile:
+                        return run(args, stdin=inputfile, stdout=outputfile)
+                except FileNotFoundError as error:
+                    # improve errors with an errors.py
+                    print(inputfile)
+                    print(outputfile)
+                    print("Error: {}".format(error))
+
