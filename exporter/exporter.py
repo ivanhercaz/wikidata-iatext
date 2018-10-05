3772fc1e6b8215d4e458f74909f49f66aecdbda9
ivanhercaz <ivan@ivanhercaz.com>
2018-10-05 12:59:04 +0100

    basic script with two command to extract the entities with a SPARQL query and wikidata-cli and then to get ttl file

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -3,12 +3,15 @@
 from exporter import utils
 
 class Exporter:
+    def __init__(self):
+            utils.checkCommand("npm")
+            utils.checkCommand("wd", commandName="wikidata-cli")
+
     def run(self):
         # Command example of wikidata-cli: wd data --format ttl Q123 Q3548931 Q515168
-       try:
-           utils.runCommand("wd", "data", "--format", "ttl", "Q123")
-       except:
-            print("adios")
+        # utils.runCommand("wd", "data", "--format", "ttl", "Q123")
+        utils.runCommand("wd", "sparql", "researchers/researchers.rq", outputfile="researchers/researchers")
+        utils.runCommand("wd", "data", "--format", "ttl", inputfile="researchers/researchers", outputfile="researchers/researchers.ttl")
 
 
 utils = utils.Utils()
