a4a7dc51b5f068cdb86eca7f9f58b6f4fdbb2393
ivanhercaz <ivan@ivanhercaz.com>
2018-10-07 11:37:28 +0100

    cleaning unnecessary and minor fixes

--- exporter/utils.py
+++ exporter/utils.py
@@ -1,14 +1,13 @@
 from subprocess import run, DEVNULL
 from subprocess import CalledProcessError
 
-import re, shutil, sys, time
+import re, sys, time
 
 class Utils:
     def timeElapsed(self, start):
         elapsed = "Elapsed time from the beginning until the last process: {}".format(
             time.strftime("%M:%S", time.gmtime(time.perf_counter() - start))
         )
-
         return elapsed
 
     def timeStart(self):
@@ -16,15 +15,18 @@ class Utils:
 
     def countItems(self, identifiers):
         identifiers = identifiers.replace(".rq", "")
-        with open(identifiers, "r") as identifiersFile:
-            identifiersContent = identifiersFile.read()
+
+        try:
+            with open(identifiers, "r") as identifiersFile:
+                identifiersContent = identifiersFile.read()
+
+        except FileNotFoundError as error:
+            return "Error: {}".format(error)
 
         return len(re.findall("Q", identifiersContent))
 
     def prepareQuery(self, query):
         queryDuplicated = re.sub(r"\.rq", "-duplicated.rq", query)
-
-        # shutil.copyfile(query, queryDuplicated)
 
         with open(query, "r") as queryFile:
             queryContent = queryFile.read()
@@ -36,16 +38,7 @@ class Utils:
         with open(queryDuplicated, "w") as queryFile:
             queryFile.write(replacement)
 
-        query = queryDuplicated
-
-        return query
-        """
-        with open(queryDuplicated, "r+") as queryFile:
-            print("query abierta")
-            queryRead = queryFile.read()
-            replacement = re.sub(r" \?item[A-Z]\w*", "", str(queryRead))
-            queryFile.write(replacement)
-        """
+        return queryDuplicated
 
     def removeQuery(self, query):
         self.runCommand("rm", query)
@@ -70,26 +63,25 @@ class Utils:
             sys.exit()
 
     def runCommand(self, *args, inputfile="", outputfile=""):
-        print("outputfile: {}".format(outputfile))
-        print("inputfile: {}".format(inputfile))
-
         if outputfile is not "":
-            if inputfile is not "": 
+            print("Output file: {}".format(outputfile))
+
+            if inputfile is not "":
+                print("Input file: {}".format(inputfile))
+
                 if inputfile.endswith(".rq") is False: 
                     countItems = self.countItems(inputfile)
-                    print("Items (Qxxx) in the identifiers file".format(countItems))
+                    print("Items (Qxxx) in the identifiers file ({}): {} items".format(inputfile, countItems))
 
                 try:
                     with open(inputfile, "r") as inputfile, open(outputfile, "w") as outputfile:
-                        print(args)
                         return run(args, stdin=inputfile, stdout=outputfile)
 
                 except FileNotFoundError as error:
                     # improve errors with an errors.py
-                    print("Error: {}".format(error))
+                    return "Error: {}".format(error)
             try:
                 with open(outputfile, "w") as outputfile:
-                    print(args)
                     return run(args, stdout=outputfile)
 
             except FileNotFoundError as error:
