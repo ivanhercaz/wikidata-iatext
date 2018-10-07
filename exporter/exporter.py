5a1626eaef258a7887e357f9af62e5c6e7a9ed41
ivanhercaz <ivan@ivanhercaz.com>
2018-10-07 11:22:09 +0100

    print of the time elapsed after each query

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -8,17 +8,20 @@ class Exporter:
             utils.checkCommand("wd", commandName="wikidata-cli")
 
     def run(self):
+        start = utils.timeStart()
         # Command example of wikidata-cli: wd data --format ttl Q123 Q3548931 Q515168
         # utils.runCommand("wd", "data", "--format", "ttl", "Q123")
         query = utils.prepareQuery("researchers/researchers.rq")
         utils.runCommand("wd", "sparql", query, outputfile="researchers/researchers")
         utils.runCommand("wd", "data", "--format", "ttl", inputfile="researchers/researchers", outputfile="researchers/researchers.ttl")
         utils.removeQuery(query)
+        print(utils.timeElapsed(start))
 
         query = utils.prepareQuery("publications/publications.rq")
         utils.runCommand("wd", "sparql", query, outputfile="publications/publications")
         utils.runCommand("wd", "data", "--format", "ttl", inputfile="publications/publications", outputfile="publications/publications.ttl")
         utils.removeQuery(query)
+        print(utils.timeElapsed(start))
 
 utils = utils.Utils()
 
