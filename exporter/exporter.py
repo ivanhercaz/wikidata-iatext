2b7b140acf580190aa10ae30b54ad27882ca2473
ivanhercaz <ivan@ivanhercaz.com>
2018-10-08 22:14:26 +0100

    adding instructions and adapting the code to PEP8

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -3,28 +3,33 @@
 from exporter import utils
 
 class Exporter:
+    utils = utils.Utils()
+
     def __init__(self):
-            utils.checkCommand("npm")
-            utils.checkCommand("wd", commandName="wikidata-cli")
+        print("- Script to export the Wikidata items worked in the project wikidata-iatext to"
+              " RDF/Turtle (.ttl).\n")
+        print("Checking that everything is installed")
+        self.utils.checkCommand(["node", "--version"], commandName="node")
+        self.utils.checkCommand("npm")
+        self.utils.checkCommand("wd", commandName="wikidata-cli")
 
     def run(self):
-        start = utils.timeStart()
+        start = self.utils.timeStart()
         # Command example of wikidata-cli: wd data --format ttl Q123 Q3548931 Q515168
-        # utils.runCommand("wd", "data", "--format", "ttl", "Q123")
-        query = utils.prepareQuery("researchers/researchers.rq")
-        utils.runCommand("wd", "sparql", query, outputfile="researchers/researchers")
-        utils.runCommand("wd", "data", "--format", "ttl", inputfile="researchers/researchers", outputfile="researchers/researchers.ttl")
-        utils.removeQuery(query)
-        print(utils.timeElapsed(start))
+        # self.utils.runCommand("wd", "data", "--format", "ttl", "Q123")
+        query = self.utils.prepareQuery("researchers/researchers.rq")
+        self.utils.runCommand("wd", "sparql", query, outputfile="researchers/researchers")
+        self.utils.runCommand("wd", "data", "--format", "ttl", inputfile="researchers/researchers",
+                              outputfile="researchers/researchers.ttl")
+        self.utils.removeQuery(query)
+        print("{}\n".format(self.utils.timeElapsed(start)))
 
-        query = utils.prepareQuery("publications/publications.rq")
-        utils.runCommand("wd", "sparql", query, outputfile="publications/publications")
-        utils.runCommand("wd", "data", "--format", "ttl", inputfile="publications/publications", outputfile="publications/publications.ttl")
-        utils.removeQuery(query)
-        print(utils.timeElapsed(start))
+        query = self.utils.prepareQuery("publications/publications.rq")
+        self.utils.runCommand("wd", "sparql", query, outputfile="publications/publications")
+        self.utils.runCommand("wd", "data", "--format", "ttl", inputfile="publications/publications",
+                         outputfile="publications/publications.ttl")
+        self.utils.removeQuery(query)
+        print("{}\n".format(self.utils.timeElapsed(start)))
 
-utils = utils.Utils()
-
-# To test
-# exporter = Exporter()
-# exporter.run()
+        print("Export finished. Your RDF/Turtle datasets are their respective folders"
+              " (publications and researchers).")
